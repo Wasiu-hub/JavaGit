a3363bfaec82353a11b3be11a33d4755559444ef
Wasiu-hub <69483917+Wasiu-hub@users.noreply.github.com>
2022-02-07 23:14:45 +0000

    Update GreatestCommonDivisor.java

--- GreatestCommonDivisor/GreatestCommonDivisor.java
+++ GreatestCommonDivisor/GreatestCommonDivisor.java
@@ -1,4 +1,4 @@
-    /** 20 Greatest Common Divisor
+    /** Greatest Common Divisor
      Write a method named getGreatestCommonDivisor with two parameters of type int named first and second.
      If one of the parameters is < 10, the method should return -1 to indicate an invalid value.
      The method should return the greatest common divisor of the two numbers (int).
