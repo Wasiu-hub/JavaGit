e7289bcc99f4655b89196286d8d1aafbf43ee825
Wasiu-hub <69483917+Wasiu-hub@users.noreply.github.com>
2022-03-26 02:21:27 +0000

    Update PaintJob.java

--- PaintJob.java
+++ PaintJob.java
@@ -5,11 +5,15 @@
  1. Write a method named getBucketCount with 4 parameters. The first parameter should be named width of type double. This parameter represents the 
  width of the wall. The second parameter should be named height of type double. This parameter represents the height of the wall. The third parameter 
  should be named areaPerBucket. This parameter represents the area that can be covered with one bucket of paint. The fourth parameter should be named 
- extraBuckets. This parameter represents the bucket count that Bob has at home. The method needs to return a value of type int that represents the number of buckets that Bob needs to buy before going to work. To calculate the bucket count, refer to the notes below. If one of the parameters width, height or areaPerBucket is less or equal to 0 or if extraBuckets is less than 0, the method needs to return -1 to indicate an invalid value. If all parameters are valid, the method needs to calculate the number of buckets and return it.
+ extraBuckets. This parameter represents the bucket count that Bob has at home. The method needs to return a value of type int that represents the number 
+ of buckets that Bob needs to buy before going to work. To calculate the bucket count, refer to the notes below. If one of the parameters width, height 
+ or areaPerBucket is less or equal to 0 or if extraBuckets is less than 0, the method needs to return -1 to indicate an invalid value. If all parameters 
+ are valid, the method needs to calculate the number of buckets and return it.
  
- 2. Bob does not like to enter 0 for the extraBuckets parameter so he needs another method. Write another overloaded method named getBucketCount with 3 
- parameters namely width, height, and areaPerBucket (all of type double). This method needs to return a value of type int that represents the number 
- of buckets that Bob needs to buy before going to work. To calculate the bucket count, refer to the notes below. If one of the parameters width, height
+ 2. Bob does not like to enter 0 for the extraBuckets parameter so he needs another method. Write another overloaded method named getBucketCount 
+ with 3 parameters namely width, height, and areaPerBucket (all of type double). This method needs to return a value of type int that represents the 
+ number of buckets that Bob needs to buy before going to work. To calculate the bucket count, refer to the notes below. If one of the parameters width, 
+ height
  or areaPerBucket is less or equal to 0, the method needs to return -1 to indicate an invalid value. If all parameters are valid, the method needs to 
  calculate the number of buckets and return it.
  */
