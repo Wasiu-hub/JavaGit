1f1e5190b7f8aa25c59c251b46ea22fb64d00eb3
Wasiu-hub <contactbow@yahoo.com>
2022-03-26 03:37:02 +0000

    ex_divisibleByThree

---
+++ DivisibleByThree/DivisibleByThree.java
@@ -0,0 +1,16 @@
+// Write a method public static void divisibleByThreeInRange(int beginning, int end) that prints all the numbers divisible
+// by three in the given range. The numbers are to be printed in order from the smallest to the greatest.
+class Main1 {
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        divisibleByThree(2,10);
+    }
+    public static void divisibleByThree(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
