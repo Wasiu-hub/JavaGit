be58a1a6e8adeb69e71f2c5ab8601a1c160e1b9c
Wasiu-hub <contactbow@yahoo.com>
2022-03-28 22:37:04 +0100

    ex_SecondPlusThird

---
+++ SecondPlusThird.java
@@ -0,0 +1,23 @@
+/:/ Write a program that reads integers from the user and adds them to a list, this ends when the user enters 0, and find the sum of the second and third elements. The program is allowed to malfunction if there are fewerthan three entries on the list
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SecondPlusThird {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+            }
+
+            numbers.add(number);
+        }
+        int sum = (numbers.get(1) + numbers.get(2));
+        System.out.println(sum);
+    }
+}.
