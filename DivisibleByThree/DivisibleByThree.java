dee51bc78b80ac47721d78615bd847226a6c0f42
Wasiu-hub <contactbow@yahoo.com>
2022-04-29 21:06:28 +0100

    reduntant line deleted

--- DivisibleByThree/DivisibleByThree.java
+++ DivisibleByThree/DivisibleByThree.java
@@ -1,8 +1,6 @@
 // Write a method public static void divisibleByThreeInRange(int beginning, int end) that prints all the numbers divisible
 // by three in the given range. The numbers are to be printed in order from the smallest to the greatest.
 class Main1 {
-    public static Scanner scan = new Scanner(System.in);
-
     public static void main(String[] args) {
         divisibleByThree(2,10);
     }
